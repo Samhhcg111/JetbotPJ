7f6300f2ba119157a6fab56898bad00c72bc133e
Samhhcg111 <samhong11@gmail.com>
2022-04-30 18:31:21 +0800

    1. add pause 2. more stages

--- lib/StageSwitch.py
+++ lib/StageSwitch.py
@@ -1,26 +1,28 @@
 
 class StageSwitch:
-    def __init__(self):
+    def __init__(self,TotalStage:int = 4):
         self.__stage = 1
+        self.__TotalStage = TotalStage
+        self.__pause = False
     def nextStage(self):
+        self.__pause = False
         self.__stage+=1
-        if self.__stage >3:
+        if self.__stage >self.__TotalStage:
             self.__stage=1
         self.printStage()
+
     def setStage(self,stage:int):
         self.__stage=stage
 
-    def isStage1(self):
-        if self.__stage == 1:
+    def setPause(self):
+        self.__pause =True
+
+    def isStage(self,number:int):
+        if self.__pause:
+            return False
+        if self.__stage == number:
             return True
         return False
-    def isStage2(self):
-        if self.__stage == 2:
-            return True
-        return False
-    def isStage3(self):
-        if self.__stage == 3:
-            return True
-        return False
+
     def printStage(self):
         print('Stage : '+str(self.__stage))
